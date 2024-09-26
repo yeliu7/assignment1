a452f6850e31eb6e4c3bd86d6d72c2ec4838fc72
yeliu7 <yeliu7@cmail.carleton.ca>
2024-09-26 11:00:33 -0400

    Update kickstarter_campaign.py

--- kickstarter_campaign.py
+++ kickstarter_campaign.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 st.title('kickstarter campaign overview')
 
 # Load some data
-df = pd.read_csv("/Users/clareliu/desktop/py4e/kickstarter_2016.csv")
+df = pd.read_csv("https://github.com/yeliu7/assignment1/blob/main/kickstarter_2016.csv")
 
 # Show the dataset
 st.write(df)
